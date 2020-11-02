8e5ae11f7fe9a3d9f0f9112e0373fa38e0b6ae15
Gianpaolo Torino <ravndeveloper@gmail.com>
2020-11-02 13:56:14 +0100

    fix bugs

--- js/indexJS.js
+++ js/indexJS.js
@@ -2,221 +2,228 @@ var elementInsideUS = $('#containerDaLevare')
 var elementHeroBody = $('#containerText')
 
 $(document).ready(function () {
-    window.scrollTo(0, 0)
+        window.scrollTo(0, 0)
 
 
-    var app,
-        video = document.createElement('video'),
-        divVideo = document.querySelector('#divVideo'),
-        oneTime = false;
-    video.loop = true;
-    video.crossOrigin = 'anonymous';
-    video.preload = 'auto';
-    video.src = 'media/video/VideoNoText.mp4';
-    video.autoload = true;
-    video.muted = true;
-    video.playsinline = "playsinline";
+        var app,
+            video = document.createElement('video'),
+            divVideo = document.querySelector('#divVideo'),
+            oneTime = false;
+        video.loop = true;
+        video.crossOrigin = 'anonymous';
+        video.preload = 'auto';
+        video.src = '../media/video/VideoNoText.mp4';
+        video.autoload = true;
+        video.muted = true;
+        video.playsinline = "playsinline";
 
 
-    function disableScroll() {
-        document.body.style.overflow = 'hidden';
-    }
+        function disableScroll() {
+            document.body.style.overflow = 'hidden';
+        }
 
-    function enableScroll() {
-        document.body.style.overflow = '';
-    }
+        function enableScroll() {
+            document.body.style.overflow = '';
+        }
 
+        let displacementSprite
+        let displacementFilter
+        let isFinished
+        let heightWindow = $(window).height()
+        let widthWindow = $(window).width()
 
-    function initPixi() {
-        var isFinished = false;
-        app = new PIXI.Application({width: window.innerWidth, height: window.innerHeight, resizeTo: window});
-        var videoSprite = new PIXI.Sprite.from(video);
-        const container = new PIXI.Container();
-        videoSprite.width = app.screen.width;
-        videoSprite.height = app.screen.height;
-        app.stage.addChild(container);
-        container.addChild(videoSprite)
-        displacementSprite = new PIXI.Sprite.from("media/img/clouds.jpg");
-        displacementFilter = new PIXI.filters.DisplacementFilter(displacementSprite);
-        displacementSprite.texture.baseTexture.wrapMode = PIXI.WRAP_MODES.REPEAT;
-        app.stage.addChild(displacementSprite);
-        app.stage.filters = [displacementFilter];
-        app.renderer.view.style.transform = 'scale(1)';
-        displacementSprite.scale.x = 0;
-        displacementSprite.scale.y = 0;
-        displacementSprite.position.x = app.screen.width / 2;
-        displacementSprite.position.y = app.screen.height / 2;
+        function initPixi() {
+            isFinished = false;
+            app = new PIXI.Application({
+                width: widthWindow,
+                height: heightWindow,
+            });
+            const videoTexture = new PIXI.Texture.from(video)
+            const videoSprite = new PIXI.Sprite.from(videoTexture);
+            const container = new PIXI.Container();
+            videoSprite.width = widthWindow
+            videoSprite.height = heightWindow;
+            app.stage.addChild(container);
+            container.addChild(videoSprite);
+            displacementSprite = new PIXI.Sprite.from("media/img/clouds.jpg");
+            displacementFilter = new PIXI.filters.DisplacementFilter(displacementSprite);
+            displacementSprite.texture.baseTexture.wrapMode = PIXI.WRAP_MODES.REPEAT;
+            app.stage.addChild(displacementSprite);
+            app.stage.filters = [displacementFilter];
+            app.renderer.view.style.transform = 'scale(1)';
+            displacementSprite.scale.x = 0;
+            displacementSprite.scale.y = 0;
+            displacementSprite.position.x = app.screen.width / 2;
+            displacementSprite.position.y = app.screen.height / 2;
+            tl3.to('#containerText', {
+                delay: 2,
+                duration: 2,
+                opacity: 1,
 
-        app.ticker.add((delta) => {
-            $(window).resize(function () {
-                videoSprite.width = app.screen.width;
-                videoSprite.height = app.screen.height;
             })
-        });
+            app.ticker.stop();
+            gsap.ticker.add(() => {
+                app.ticker.update();
+            });
+
+            tl3.to('.consultingText', {duration: 0.5, opacity: 1})
+            ScrollTrigger.create({
+                trigger: "#section-animated",
+                start: '10',
+                end: '3000',
+                markers: false,
+                pin: true,
+                scrub: false,
+                onEnter: self => {
+                    var tl = gsap.timeline();
+
+                    tl.to(window, {duration: 1, scrollTo: {x: 0, y: 3010}})
+                        .to(displacementSprite.scale, {duration: 1, x: '+=10', y: '+=10'}, 0)
+                        .to(displacementFilter.scale, {duration: 1, x: "+=900", y: "+=900"}, 0)
+                        .to('.hero-video', {css: {autoAlpha: 0}, duration: 1}, 0.7)
+                        .to('.hero-body', {css: {autoAlpha: 0}, duration: 1}, 0.7)
+                        .add(function () {
+                            if (isFinished === false) {
+                                $('.hero-body').addClass('hide');
+                                $('#containerText').remove();
+                                $('#replacement').append(elementInsideUS)
+                                isFinished = true;
+
+                            }
+
+                        })
+                        .to('#replacement', {
+                            duration: 1,
+                            css: {scaleX: 1, scaleY: 1, autoAlpha: 1},
+                            ease: "power4.out"
+                        }, 1.3)
+                        .add(function () {
+                            if (oneTime === false) {
+                                animateElements()
+                                oneTime = true;
+
+                            }
+
+                        })
+                },
+                onEnterBack: self => {
+                    var tl2 = gsap.timeline();
+                    tl2.to(window, {duration: 1, scrollTo: {x: 0, y: 0}})
+                        .to('#replacement', {
+                            duration: 1,
+                            css: {scaleX: 0.2, scaleY: 0.2, autoAlpha: 0},
+                            ease: "power4.out"
+                        }, 0)
+                        .add(function () {
+                            if (isFinished === true) {
+                                console.log('animateBack')
+                                console.log(isFinished)
+                                $('#containerDaLevare').remove();
+                                $('.hero-body').append(elementHeroBody);
+                                $('.hero-body').removeClass('hide');
+                                isFinished = false;
+                            }
+                        })
+                        .to('.hero-video', {css: {autoAlpha: 1}, duration: 1}, 0.5)
+                        .to('.hero-body', {css: {autoAlpha: 1}, duration: 2}, 0.5)
+                        .to(displacementSprite.scale, {duration: 1, x: '-=10', y: '-=10'}, 1)
+                        .to(displacementFilter.scale, {duration: 1, x: "-=900", y: "-=900"}, 1)
+
+                },
+            })
+        }
 
 
-        var scrollTrigger = ScrollTrigger.create({
-            trigger: "#section-animated",
-            start: '1',
-            end: '3000',
-            markers: false,
-            pin: true,
-            scrub: false,
-            onEnter: self => {
-                var tl = gsap.timeline();
-                tl.add(function () {
-                    disableScroll()
-                })
-                    .to(window, {duration: 1, scrollTo: {x: 0, y: 3005}})
-                    .to(displacementSprite.scale, {duration: 1, x: '+=10', y: '+=10', ease: Power0.easeInOut}, 0)
-                    .to(displacementFilter.scale, {duration: 1, x: "+=900", y: "+=900", ease: Power0.easeInOut}, 0)
-                    .to('.hero-video', {css: {opacity: 0}, duration: 1}, 0.7)
-                    .to('.hero-body', {css: {opacity: 0}, duration: 1}, 0.7)
-                    .add(function () {
-                        if (isFinished === false) {
-                            $('.hero-body').addClass('hide');
-                            $('#containerText').remove();
-                            $('#replacement').append(elementInsideUS)
-                            isFinished = true;
-
-                        }
-
-                    })
-                    .to('#replacement', {duration: 1, css: {scaleX: 1, scaleY: 1, opacity: 1}, ease: "power4.out"},1.3)
-                    .add(function () {
-                        if (oneTime === false) {
-                            animateElements()
-                            oneTime = true;
-
-                        }
-
-                        enableScroll()
-
-                    })
-            },
-            onEnterBack: self => {
-                var tl2 = gsap.timeline();
-                tl2.add(function () {
-                    disableScroll()
-                })
-                    .to(window, {duration: 1, scrollTo: {x: 0, y: 0}})
-                    .to('#replacement', {
-                        duration: 1,
-                        css: {scaleX: 0.2, scaleY: 0.2, opacity: 0},
-                        ease: "power4.out"
-                    }, 0)
-                    .add(function () {
-                        if (isFinished === true) {
-                            console.log('animateBack')
-                            console.log(isFinished)
-                            $('#containerDaLevare').remove();
-                            $('.hero-body').append(elementHeroBody);
-                            $('.hero-body').removeClass('hide');
-                            isFinished = false;
-                        }
-                    })
-                    .to('.hero-video', {css: {opacity: 1}, duration: 1}, 0.5)
-                    .to('.hero-body', {css: {opacity: 1}, duration: 1}, 0.5)
-                    .to(displacementSprite.scale, {duration: 1, x: '-=10', y: '-=10', ease: Power0.easeInOut}, 1)
-                    .to(displacementFilter.scale, {duration: 1, x: "-=900", y: "-=900", ease: Power0.easeInOut}, 1)
-                    .add(function () {
-                        enableScroll()
-                    })
-
-            },
-        })
 
 
-    }
+        var finished = false
+        const tl3 = gsap.timeline();
 
-    var finished = false
-    const tl3 = gsap.timeline();
-
-    if (!sessionStorage.getItem('doNotShow')) {
-        //sessionStorage.setItem( 'doNotShow', true );
-        loader()
-    } else {
-        tl3.to('.pageloader', {
-            duration: 0.5, opacity: 0, onComplete: function () {
-                $('.pageloader').hide();
-            }
-        })
-
-        if ($(window).width() > 1024) {
-            initPixi();
-            divVideo.appendChild(app.view);
+        if (!sessionStorage.getItem('doNotShow')) {
+            sessionStorage.setItem('doNotShow', true);
+            loader()
         } else {
-            animateElements()
-            $('.gradient-video').css('height', $('.cardinoVideo').height() + 'px')
-        }
-    }
-
-    function loader() {
-        var pageloader;
-        pageloader = $('.pageloaderLogo').find('circle')
-        for (let i = 0; i < 20; i+=2) {
-
-            for (let k = 0; k<20; k++){
-                var cx2 = $(pageloader[k]).attr('cx'),
-                    cy2 = $(pageloader[k]).attr('cy');
-                tl3.set(pageloader[k], {attr: {style: 'transform-origin: ' + cx2 + 'px ' + cy2 + 'px'}})
-            }
-            tl3.to(pageloader[i], {duration: 0.1, attr: {transform: 'matrix(1,0,0,1,0,0)'}, ease: 'power2.in'});
-            tl3.to(pageloader[i+1], {duration: 0.1, attr: {transform: 'matrix(1,0,0,1,0,0)'}, ease: 'power2.in'});
-            if (i === 20) {
-                finished = true
-            }
-        }
-
-        var makuda = ['M', 'A_2', 'K', 'U', 'D', 'A', 'triangle']
-        for (let k = 0; k < makuda.length; k++) {
-
-            tl3.to('#' + makuda[k], {
-                duration: 0, opacity: 1, onComplete: function () {
-
-                    //$('#pageloaderImg').css('opacity','1')
-/*
-                    $(pageloader).remove()
-                    for (let l = 5;l>2;l--){
-                        tl3.to('#'+makuda[l],{duration:0.2 ,scaleY:0, ease: 'power2.in'})
-                    }
-                    for (let z = 0;z<2;z++){
-                        tl3.to('#'+makuda[z],{duration:0.2,scaleY:0, ease: 'power2.in'})
-                    }
-
-
- */
-
+            tl3.to('.pageloader', {
+                duration: 0.5, autoAlpha: 0, onComplete: function () {
+                    $('.pageloader').hide();
                 }
             })
+            if ($(window).width() > 1024) {
 
-
-        }
-
-
-
-
-        if ($(window).width() > 1024) {
-            tl3.add(function () {
                 initPixi();
+                tl3.to('.pageloader', {
+                    duration: 0.5, autoAlpha: 0, onComplete: function () {
+                        $('.pageloader').hide();
+                    }
+                })
                 divVideo.appendChild(app.view);
-            }, 3);
-        } else {
-            animateElements()
-            $('.gradient-video').css('height', $('.cardinoVideo').height() + 'px')
+            } else {
+                animateElements()
+                $('.gradient-video').css('height', $('.cardinoVideo').height() + 'px')
+            }
         }
 
-            tl3.to('.pageloader', {duration: 1, css: {opacity: 0}})
-            tl3.to('.pageloader', {duration: 1, css: {display: 'none'}});
+        function loader() {
+            var pageloader;
+            pageloader = $('.pageloaderLogo').find('circle')
+            for (let i = 0; i < 20; i += 2) {
+
+                for (let k = 0; k < 20; k++) {
+                    var cx2 = $(pageloader[k]).attr('cx'),
+                        cy2 = $(pageloader[k]).attr('cy');
+                    tl3.set(pageloader[k], {attr: {style: 'transform-origin: ' + cx2 + 'px ' + cy2 + 'px'}})
+                }
+                tl3.to(pageloader[i], {duration: 0.1, attr: {transform: 'matrix(1,0,0,1,0,0)'}, ease: 'power2.in'});
+                tl3.to(pageloader[i + 1], {duration: 0.1, attr: {transform: 'matrix(1,0,0,1,0,0)'}, ease: 'power2.in'});
+                if (i === 20) {
+                    finished = true
+                }
+            }
+
+            var makuda = ['M', 'A_2', 'K', 'U', 'D', 'A', 'triangle']
+            for (let k = 0; k < makuda.length; k++) {
+
+                tl3.to('#' + makuda[k], {
+                    duration: 0, autoAlpha: 1, onComplete: function () {
+
+                        //$('#pageloaderImg').css('opacity','1')
+                        /*
+                                            $(pageloader).remove()
+                                            for (let l = 5;l>2;l--){
+                                                tl3.to('#'+makuda[l],{duration:0.2 ,scaleY:0, ease: 'power2.in'})
+                                            }
+                                            for (let z = 0;z<2;z++){
+                                                tl3.to('#'+makuda[z],{duration:0.2,scaleY:0, ease: 'power2.in'})
+                                            }
 
 
+                         */
+
+                    }
+                })
 
 
+            }
+
+
+            if ($(window).width() > 1024) {
+                tl3.add(function () {
+                    initPixi();
+                    divVideo.appendChild(app.view);
+                }, 3);
+            } else {
+                animateElements()
+                $('.gradient-video').css('height', $('.cardinoVideo').height() + 'px')
+            }
+
+            tl3.to('.pageloader', {duration: 1, css: {autoAlpha: 0}})
+            tl3.to('.pageloader', {duration: 1, css: {display: 'none'}}, '-=0.5');
+
+
+        }
 
 
     }
-
-
-})
+)
 
 
--- js/menu.js
+++ js/menu.js
@@ -1,98 +1,127 @@
-(function () {
-    const blurProperty = gsap.utils.checkPrefix("filter"),
-        blurExp = /blur\((.+)?px\)/,
-        getBlurMatch = target => (gsap.getProperty(target, blurProperty) || "").match(blurExp) || [];
+var menuAnimationFinished = false;
+var heightTrinagle = $('#triangleMenu').attr('height') / 2;
+var heightTotal = ($(window).height() / 2) - heightTrinagle
+$('#triangleMenu').css({'transform': 'translateY(' + heightTotal + 'px)' + ' rotate(-90deg) scale(5)'});
 
-    gsap.registerPlugin({
-        name: "blur",
-        get(target) {
-            return +(getBlurMatch(target)[1]) || 0;
-        },
-        init(target, endValue) {
-            let data = this,
-                filter = gsap.getProperty(target, blurProperty),
-                endBlur = "blur(" + endValue + "px)",
-                match = getBlurMatch(target)[0],
-                index;
-            if (filter === "none") {
-                filter = "";
+$(function () {
+    (function () {
+        const blurProperty = gsap.utils.checkPrefix("filter"),
+            blurExp = /blur\((.+)?px\)/,
+            getBlurMatch = target => (gsap.getProperty(target, blurProperty) || "").match(blurExp) || [];
+
+        gsap.registerPlugin({
+            name: "blur",
+            get(target) {
+                return +(getBlurMatch(target)[1]) || 0;
+            },
+            init(target, endValue) {
+                let data = this,
+                    filter = gsap.getProperty(target, blurProperty),
+                    endBlur = "blur(" + endValue + "px)",
+                    match = getBlurMatch(target)[0],
+                    index;
+                if (filter === "none") {
+                    filter = "";
+                }
+                if (match) {
+                    index = filter.indexOf(match);
+                    endValue = filter.substr(0, index) + endBlur + filter.substr(index + match.length);
+                } else {
+                    endValue = filter + endBlur;
+                    filter += filter ? " blur(0px)" : "blur(0px)";
+                }
+                data.target = target;
+                data.interp = gsap.utils.interpolate(filter, endValue);
+            },
+            render(progress, data) {
+                data.target.style[blurProperty] = data.interp(progress);
             }
-            if (match) {
-                index = filter.indexOf(match);
-                endValue = filter.substr(0, index) + endBlur + filter.substr(index + match.length);
-            } else {
-                endValue = filter + endBlur;
-                filter += filter ? " blur(0px)" : "blur(0px)";
+        });
+    })();
+})
+var letters = $('.letterWrapper');
+letters.on('mouseenter', function (event) {
+
+    var lettersLength = $(this).find('.letter').length;
+    var numberGenerate;
+    var numberGenerated = [];
+    for (let i = 0; i < lettersLength; i++) {
+        var tl = gsap.timeline();
+
+        do {
+            var uscire = false
+            numberGenerate = Math.floor(Math.random() * (lettersLength));
+            if (!numberGenerated.includes(numberGenerate)) {
+                numberGenerated.push(numberGenerate)
+                uscire = true
             }
-            data.target = target;
-            data.interp = gsap.utils.interpolate(filter, endValue);
-        },
-        render(progress, data) {
-            data.target.style[blurProperty] = data.interp(progress);
-        }
-    });
-})();
+        } while (!uscire)
+        var numberGeneratedI = numberGenerated[i].toFixed(0)
+
+        tl.to($(this).children().children()[numberGeneratedI], {
+            duration: 0.5,
+            rotationX: 100,
+            z: -200,
+            transformOrigin: '50% 50% -26rem',
+            ease: 'Power4.easeIn',
+        }, '0.' + (i))
+
+
+        tl.set($(this).children().children()[numberGeneratedI], {
+            rotationX: -90,
+            z: -200,
+            transformOrigin: '50% 50% -26rem',
+            ease: 'Power4.easeOut',
+        })
+        tl.to($(this).children().children()[numberGeneratedI], {
+            duration: 0.7,
+            rotationX: 0,
+            z: 0,
+            transformOrigin: '50% 50% -26rem',
+            ease: 'Power4.easeOut',
+        })
+    }
+
+})
+
 
 function animateMenu() {
     var tl = gsap.timeline();
-    tl.to('.menu-background', {duration: 1, translateX: '-90vw', translateY: '90vh', scale: 50, ease: 'power4.in'})
-        .to('.menuWrapper', {duration: 0.2, opacity: 1, display: 'flex'})
-        .to('.buttonMenu', {duration: 0.2, translateY: '45%'})
-        .to('.letter', {duration: 0.5, opacity: 1, blur: 0, translateY: 0})
+    tl.to('.menu-background', {
+        duration: 1,
+        translateX: '-' + $(window).width() + 'px',
+        display: 'initial',
+        ease: 'expo.in'
+    }, 0)
+        .to('.menuWrapper', {duration: 0.2, autoAlpha: 1, display: 'flex'})
+        .to('.letter', {duration: 0.5, autoAlpha: 1, blur: 0, translateX: 0})
 
-
-    tl.add(function () {
-        var letters = $('.letterWrapper');
-        letters.one('mouseover', function (event) {
-            var lettersLength = $(this).find('.letter').length;
-            var numberGenerate;
-            var numberGenerated = [];
-            for (let i = 0; i < lettersLength; i++) {
-                tl = gsap.timeline();
-
-                do {
-                    var uscire = false
-                    numberGenerate = Math.floor(Math.random() * (lettersLength));
-                    if (!numberGenerated.includes(numberGenerate)) {
-                        numberGenerated.push(numberGenerate)
-                        uscire = true
-                    }
-                } while (!uscire)
-                var numberGeneratedI = numberGenerated[i].toFixed(0)
-                tl.to($(this).children()[numberGeneratedI], {
-                    duration: 0.3,
-                    translateY: '40%',
-                    rotateX: 60,
-                    transformOrigin: "center center",
-                }, '0.' + (i))
-                tl.to($(this).children()[numberGeneratedI], {
-                    duration: 0.3,
-                    opacity: 0,
-                    scaleY: 0.2,
-                }, '-=0.25')
-                tl.set($(this).children()[numberGeneratedI], {
-                    translateY: '-30%',
-                    opacity: 0,
-                    transformOrigin: "center center"
-                })
-                tl.to($(this).children()[numberGeneratedI], {duration: 0.3, scaleY: 1})
-                tl.to($(this).children()[numberGeneratedI], {
-                    duration: 0.3,
-                    translateY: 0,
-                    rotateX: 0,
-                    opacity: 1,
-                    transformOrigin: "center center",
-                }, '-=0.25')
-            }
-        })
-    })
 }
 
 function animateMenuReverse() {
     var tl = gsap.timeline();
-        tl.to('.letter', {duration: 0.5, opacity: 0, blur: 10, translateY: '-45%'},0)
-            .to('.buttonMenu', {duration: 0.2, translateY: '0%'})
-            .to('.menuWrapper', {duration: 0.2, opacity: 0 ,display: 'none'})
-            .to('.menu-background', {duration: 1, translateX: '100%', translateY: '-100%', scale: 1, ease: 'power4.out'})
+    tl.to('.letter', {duration: 0.5, autoAlpha: 0, blur: 10, translateX: '+45%'}, 0)
+        .to('.menuWrapper', {duration: 0.2, autoAlpha: 0, display: 'none'})
+        .to('.menu-background', {
+            duration: 1,
+            translateX: '30vw',
+            scale: 1,
+            display: 'none',
+            ease: 'expo.in'
+        }, '-=0.5')
+        .to('.letter', {translateX: '-45%'})
+}
+
+function menuSize() {
+
+    let button = $('.buttonMenu')
+    let buttonWidth = $(window).width() / button.length + 1
+    let lettersHeight = $('.letter').width()
+    for (let i = 0; i < button.length; i++) {
+        $(button[i]).css({'margin-left': buttonWidth/5 + 'px', 'font-size': buttonWidth / 6 + 'px'})
+    }
+
 
 }
+
+menuSize()
