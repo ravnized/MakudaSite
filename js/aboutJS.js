6c458beea03ac82f03dded96e05dbb9f0859f34a
Gianpaolo <ravndeveloper@gmail.com>
2020-12-10 21:43:44 +0100

    Initial Release Parallelepipedi

--- js/aboutJS.js
+++ js/aboutJS.js
@@ -1,102 +1,185 @@
 $.fn.isInViewport = function () {
-  let elementTop = $(this).offset().top;
-  let elementBottom = elementTop + $(this).outerHeight();
+    let elementTop = $(this).offset().top;
+    let elementBottom = elementTop + $(this).outerHeight();
 
-  let viewportTop = $(window).scrollTop();
-  let viewportBottom = viewportTop + $(window).height();
+    let viewportTop = $(window).scrollTop();
+    let viewportBottom = viewportTop + $(window).height();
 
-  return elementBottom > viewportTop && elementTop < viewportBottom;
+    return elementBottom > viewportTop && elementTop < viewportBottom;
 };
 
 $(document).ready(function ($) {
-  $(".counter-count").each(function (index) {
-    if ($(this).isInViewport()) {
-      $(this)
-        .prop("Counter", 0)
-        .animate(
-          {
-            Counter: $(this).text(),
-          },
-          {
-            duration: 1000,
+    $(".counter-count").each(function (index) {
+        if ($(this).isInViewport()) {
+            $(this)
+                .prop("Counter", 0)
+                .animate(
+                    {
+                        Counter: $(this).text(),
+                    },
+                    {
+                        duration: 1000,
 
-            step: function (now) {
-              $(this).text(Math.ceil(now));
-            },
-          }
-        );
-    }
-  });
-
-  var images = $(".imagesStudio");
-  images.mousemove(function (e) {
-    var timeline = gsap.timeline();
-    var offset = $(this).offset();
-    var relativeX = e.pageX - offset.left;
-    var relativeY = e.pageY - offset.top;
-    var cX = $(this).width() / 2;
-    var cY = $(this).height() / 2;
-    console.log(relativeX, relativeY);
-    console.log(cX, cY);
-    var cxRelative = (cX - relativeX) / 20;
-    var cyRelative = (cY - relativeY) / 20;
-    timeline.to($(this).parent(), {
-      duration: 0.2,
-      rotateX: cyRelative,
-      rotateY: cxRelative,
+                        step: function (now) {
+                            $(this).text(Math.ceil(now));
+                        },
+                    }
+                );
+        }
     });
-  });
-  images.mouseleave(function (event) {
-    var timeline = gsap.timeline();
 
-    timeline.to($(this).parent(), { duration: 1, rotateX: 0, rotateY: 0 });
-  });
-  var alreadyClicked = false;
-  var parallelepipedo = $(".parallelepipedo");
-  var arrayPos = ["", "", "", "", ""];
+    var images = $(".imagesStudio");
+    images.mousemove(function (e) {
+        var timeline = gsap.timeline();
+        var offset = $(this).offset();
+        var relativeX = e.pageX - offset.left;
+        var relativeY = e.pageY - offset.top;
+        var cX = $(this).width() / 2;
+        var cY = $(this).height() / 2;
+        console.log(relativeX, relativeY);
+        console.log(cX, cY);
+        var cxRelative = (cX - relativeX) / 20;
+        var cyRelative = (cY - relativeY) / 20;
+        timeline.to($(this).parent(), {
+            duration: 0.2,
+            rotateX: cyRelative,
+            rotateY: cxRelative,
+        });
+    });
+    images.mouseleave(function (event) {
+        var timeline = gsap.timeline();
 
-  /*
-  parallelepipedo.click(function (e) {
-    var timer = 0;
-    var idElement = $(this).parent().attr("id");
-    var tl = gsap.timeline();
-    var parallelepipedoParent = parallelepipedo.parent();
-    
-      for (let i = 5; i >= idElement; i--) {
-        if (idElement !== $(parallelepipedoParent[i]).attr("id")) {
-          
-          tl.to(parallelepipedoParent[i], {
-            duration: 0.5,
-            x: +100+timer*100,
-            ease: "power2.in",
-          },0);
-          timer++;
+        timeline.to($(this).parent(), {duration: 1, rotateX: 0, rotateY: 0});
+    });
+    var alreadyClicked = false;
+    var parallelepipedo = $(".parallelepipedo");
+    parallelepipedo.click(function (e) {
+
+        let idElement = $(this).parent().attr("id"),
+            tl = gsap.timeline(),
+            parallelepipedParent = parallelepipedo.parent(),
+            timer = 0;
+
+        for (let i = 4; i >= idElement; i--) {
+            if (idElement !== $(parallelepipedParent[i]).attr("id")) {
+                tl.to(
+                    parallelepipedParent[i],
+                    {
+                        duration: 0.5,
+                        scale: 1,
+                        x: +100 + timer * 100,
+                        ease: "power2.in",
+                    },
+                    0
+                );
+                tl.to(
+                    $(parallelepipedParent[i]).find(".square"),
+                    {
+                        duration: 0.5,
+                        opacity: 0,
+                        width: -400,
+                        translateX: 0 + "px",
+                        display: "none",
+                        ease: "power2.in",
+                    },
+                    0
+                );
+                timer++;
+            }
         }
-      }
-      timer = 0;
-      for (let i = 0; i < idElement; i++) {
-        if (idElement !== $(parallelepipedoParent[i]).attr("id")) {
-          
-          tl.to(parallelepipedoParent[i], {
-            duration: 0.5,
-            x: -100-timer*100,
-            ease: "power2.in",
-          },0);
-          timer++;
+        timer = 0;
+        for (let i = 0; i < idElement; i++) {
+            if (idElement !== $(parallelepipedParent[i]).attr("id")) {
+                tl.to(
+                    parallelepipedParent[i],
+                    {
+                        duration: 0.5,
+                        scale: 1,
+                        x: -100 - timer * 100,
+                        ease: "power2.in",
+                    },
+                    0
+                );
+                tl.to(
+                    $(parallelepipedParent[i]).find(".square"),
+                    {
+                        duration: 0.5,
+                        opacity: 0,
+                        width: -400,
+                        translateX: 0 + "px",
+                        display: "none",
+                        ease: "power2.in",
+                    },
+                    0
+                );
+                timer++;
+            }
         }
-      }
-      
-      tl.to(
-        $(this).parent(),
-        {
-          duration: 0.5,
-          x: -idElement*100 ,
-          ease: "power2.in",
-        },
-        0
-      );
-      
-    
-  });
-  */
+
+        tl.to(
+            $(this).parent(),
+            {
+                duration: 0.5,
+                x: -idElement * 100,
+                ease: "power2.in",
+            },
+            0
+        );
+        tl.to(
+            $(this).parent(),
+            {duration: 0.5, scale: 1.2, ease: "power2.in"},
+            0
+        );
+
+        tl.to($(this).parent().find(".square"), {
+            display: "flex",
+            duration: 0.5,
+            opacity: 1,
+            width: 400,
+            translateX: 170,
+            ease: "power2.in",
+        });
+    });
+
+    parallelepipedo.hover(function (e) {
+        let secondText = $(this).find(".secondText");
+        let tl = gsap.timeline();
+
+        tl.to(
+            secondText,
+            {
+                duration: 0.5,
+
+                translateY: 300,
+                ease: "power2.in",
+            },
+            0
+        );
+        tl.to(
+            secondText,
+            {
+                duration: 0.2,
+                css: {
+                    letterSpacing: 0.2 + "em",
+                },
+            },
+            0.4
+        );
+    });
+
+    parallelepipedo.mouseleave(function (e) {
+        let secondText = $(this).find(".secondText");
+        let tl = gsap.timeline();
+
+        tl.to(
+            secondText,
+            {
+                duration: 0.5,
+                translateY: 500,
+                ease: "power2.in",
+            },
+            0
+        );
+        tl.to(secondText, {css: {letterSpacing: 0}});
+    });
 });
--- js/carouselManipulator.js
+++ js/carouselManipulator.js
@@ -1,79 +1,125 @@
-$(document).ready(function ($) {
-    var currentSlideIDReview = 1;
-    var currentSlideIDVideo = 1;
-    var carouselFinderReview = $('#carousel1');
-    var carouselFinderVideo = $('#carousel2');
-    var sliderNavigationPrevious = carouselFinderReview.find('.slider-navigation-previous');
-    var sliderNavigationNext = carouselFinderReview.find('.slider-navigation-next');
-    var sliderNavigationPrevious2 = carouselFinderVideo.find('.slider-navigation-previous');
-    var sliderNavigationNext2 = carouselFinderVideo.find('.slider-navigation-next');
+var currentSlideIDReview = 1;
+var currentSlideIDVideo = 1;
+var carouselFinderReview = $("#carousel1");
+var carouselFinderVideo = $("#carousel2");
+var sliderNavigationPrevious = carouselFinderReview.find(
+  ".slider-navigation-previous"
+);
+var sliderNavigationNext = carouselFinderReview.find(".slider-navigation-next");
+var sliderNavigationPrevious2 = carouselFinderVideo.find(
+  ".slider-navigation-previous"
+);
+var sliderNavigationNext2 = carouselFinderVideo.find(".slider-navigation-next");
 
-    sliderNavigationNext2.click(function (event) {
-        event.preventDefault();
-        currentSlideIDVideo += 1;
-        videoNextPrevious();
-    })
-
-    sliderNavigationPrevious2.click(function (event) {
-        event.preventDefault();
-        currentSlideIDVideo -= 1;
-        videoNextPrevious();
-    })
-
-    sliderNavigationNext.click(function (event) {
-        event.preventDefault();
-        currentSlideIDReview += 1;
-        reviewNextPrevious();
-    })
-
-    sliderNavigationPrevious.click(function (event) {
-        event.preventDefault();
-        currentSlideIDReview -= 1;
-        reviewNextPrevious();
-    })
-
-    function reviewNextPrevious() {
-        var currentSlideReview = carouselFinderReview.find('[data-slider-index="' + currentSlideIDReview + '"]');
-        var previousSlideIDReview = currentSlideIDReview - 1;
-        var previousSlideReview = carouselFinderReview.find('[data-slider-index="' + previousSlideIDReview + '"]');
-        var nextSlideIDReview = currentSlideIDReview + 1;
-        var nextSlideReview = carouselFinderReview.find('[data-slider-index="' + nextSlideIDReview + '"]');
-        var elementTestimonial = currentSlideReview.find('.makuda-testimonials-item');
-        var elementTestimonialGradient = currentSlideReview.find('.testimonials-gradient');
-        var elementTestimonialPrevious = previousSlideReview.find('.makuda-testimonial-item');
-        var elementTestimonialGradientPrevious = previousSlideReview.find('.testimonials-gradient');
-        var elementTestimonialNext = nextSlideReview.find('.makuda-testimonial-item');
-        var elementTestimonialGradientNext = nextSlideReview.find('.testimonials-gradient');
-        elementTestimonialGradientPrevious.addClass("testimonial-prima");
-        elementTestimonialGradientNext.addClass("testimonial-dopo");
-        elementTestimonialGradient.removeClass('testimonial-prima');
-        elementTestimonialGradient.removeClass('testimonial-dopo');
-    }
-
-    function videoNextPrevious() {
-        var currentSlideVideo = $('[data-slider-index="' + currentSlideIDVideo + '"]');
-        var previousSlideIDVideo = currentSlideIDVideo - 1;
-        var previousSlideVideo = $('[data-slider-index="' + previousSlideIDVideo + '"]');
-        var nextSlideIDVideo = currentSlideIDVideo + 1;
-        var nextSlideVideo = $('[data-slider-index="' + nextSlideIDVideo + '"]');
-        var elementVideo = currentSlideVideo.find('.cardinoVideo');
-        var elementGradient = currentSlideVideo.find('.gradient-video');
-        var elementVideoPrevious = previousSlideVideo.find('.cardinoVideo');
-        var elementGradientPrevious = previousSlideVideo.find('.gradient-video');
-        var elementVideoNext = nextSlideVideo.find('.cardinoVideo');
-        var elementGradientNext = nextSlideVideo.find('.gradient-video');
-        console.log('current: ' + currentSlideIDVideo)
-        console.log('previous: ' + previousSlideIDVideo)
-        console.log('next: ' + nextSlideIDVideo)
-
-        elementGradientPrevious.addClass("video-prima");
-        elementGradientNext.addClass("video-dopo");
-        elementGradient.removeClass('video-prima');
-        elementGradient.removeClass('video-dopo');
-    }
-
-    videoNextPrevious();
-    reviewNextPrevious();
-
-
+sliderNavigationNext2.click(function (event) {
+  event.preventDefault();
+  currentSlideIDVideo += 1;
+  videoNextPrevious();
 });
+
+sliderNavigationPrevious2.click(function (event) {
+  event.preventDefault();
+  currentSlideIDVideo -= 1;
+  videoNextPrevious();
+});
+
+sliderNavigationNext.click(function (event) {
+  event.preventDefault();
+  currentSlideIDReview += 1;
+  reviewNextPrevious();
+});
+
+sliderNavigationPrevious.click(function (event) {
+  event.preventDefault();
+  currentSlideIDReview -= 1;
+  reviewNextPrevious();
+});
+
+function reviewNextPrevious() {
+  var tl = gsap.timeline();
+  var currentSlideReview = carouselFinderReview.find(
+    '[data-slider-index="' + currentSlideIDReview + '"]'
+  );
+  var previousSlideIDReview = currentSlideIDReview - 1;
+  var previousSlideReview = carouselFinderReview.find(
+    '[data-slider-index="' + previousSlideIDReview + '"]'
+  );
+  var nextSlideIDReview = currentSlideIDReview + 1;
+  var nextSlideReview = carouselFinderReview.find(
+    '[data-slider-index="' + nextSlideIDReview + '"]'
+  );
+  var elementTestimonial = currentSlideReview.find(".wrapper-testimonial");
+  var elementTestimonialGradient = currentSlideReview.find(
+    ".testimonials-gradient"
+  );
+  var elementTestimonialPrevious = previousSlideReview.find(
+    ".wrapper-testimonial"
+  );
+  var elementTestimonialGradientPrevious = previousSlideReview.find(
+    ".testimonials-gradient"
+  );
+  var elementTestimonialNext = nextSlideReview.find(".wrapper-testimonial");
+  var elementTestimonialGradientNext = nextSlideReview.find(
+    ".testimonials-gradient"
+  );
+  tl.to($(elementTestimonial), { duration: 1, scaleX: 1, scaleY: 1 }, 0);
+  if (elementTestimonialPrevious.length != 0) {
+    tl.to(
+      $(elementTestimonialPrevious),
+      { duration: 1, scaleX: 0.7, scaleY: 0.7 },
+      0
+    );
+  }
+  if (elementTestimonialNext.length != 0) {
+    tl.to(
+      $(elementTestimonialNext),
+      { duration: 1, scaleX: 0.7, scaleY: 0.7 },
+      0
+    );
+  }
+
+  elementTestimonialGradientPrevious.addClass("testimonial-prima");
+  elementTestimonialGradientNext.addClass("testimonial-dopo");
+  elementTestimonialGradient.removeClass("testimonial-prima");
+  elementTestimonialGradient.removeClass("testimonial-dopo");
+}
+
+function videoNextPrevious() {
+  var tl = gsap.timeline();
+  var currentSlideVideo = $(
+    '[data-slider-index="' + currentSlideIDVideo + '"]'
+  );
+  var previousSlideIDVideo = currentSlideIDVideo - 1;
+  var previousSlideVideo = $(
+    '[data-slider-index="' + previousSlideIDVideo + '"]'
+  );
+  var nextSlideIDVideo = currentSlideIDVideo + 1;
+  var nextSlideVideo = $('[data-slider-index="' + nextSlideIDVideo + '"]');
+  var elementVideo = currentSlideVideo.find(".cardinoVideo");
+  var elementGradient = currentSlideVideo.find(".gradient-video");
+  var elementVideoPrevious = previousSlideVideo.find(".cardinoVideo");
+  var elementGradientPrevious = previousSlideVideo.find(".gradient-video");
+  var elementVideoNext = nextSlideVideo.find(".cardinoVideo");
+  var elementGradientNext = nextSlideVideo.find(".gradient-video");
+  console.log("current: " + currentSlideIDVideo);
+  console.log("previous: " + previousSlideIDVideo);
+  console.log("next: " + nextSlideIDVideo);
+  tl.to($(elementVideo), { duration: 1, scaleX: 1, scaleY: 1 }, 0);
+  if (elementVideoPrevious.length != 0) {
+    tl.to(
+      $(elementVideoPrevious),
+      { duration: 1, scaleX: 0.7, scaleY: 0.7 },
+      0
+    );
+  }
+  if (elementVideoNext.length != 0) {
+    tl.to($(elementVideoNext), { duration: 1, scaleX: 0.7, scaleY: 0.7 }, 0);
+  }
+  elementGradientPrevious.addClass("video-prima");
+  elementGradientNext.addClass("video-dopo");
+  elementGradient.removeClass("video-prima");
+  elementGradient.removeClass("video-dopo");
+}
+
+videoNextPrevious();
+reviewNextPrevious();
