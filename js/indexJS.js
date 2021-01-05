9d964cfeda21bb876c5a8fca58c48f8655d8150a
Gianpaolo <ravndeveloper@gmail.com>
2021-01-05 17:00:53 +0100

    resolving fix

--- js/indexJS.js
+++ js/indexJS.js
@@ -1,9 +1,14 @@
 var elementInsideUS = $("#containerDaLevare");
 var elementHeroBody = $("#containerText");
 
-window.onbeforeunload;
-
 $(document).ready(function () {
+  /*NAPO*/
+  let band_texts = ["STRATEGIA", "PRODUZIONE", "INNOVAZIONE"];
+  let band_actual_index = 0;
+  setInterval(function myMethod() {
+    $(".home-band p").html(band_texts[++band_actual_index % 3]);
+  }, 1000);
+  /*FINE NAPO*/
   var app,
     video = document.createElement("video"),
     divVideo = document.querySelector("#divVideo"),
@@ -53,7 +58,7 @@ $(document).ready(function () {
       autoAlpha: 1,
     });
     tl3.to(".consultingText", { duration: 0.5, autoAlpha: 1 });
-    tl3.to(".consultingBtn", { duration: 0.5, autoAlpha: 1 });
+    tl3.to(".animatedBtn", { duration: 0.5, autoAlpha: 1 });
 
     app.ticker.stop();
     gsap.ticker.add(() => {
