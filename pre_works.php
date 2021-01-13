0b7c5c8ad545c86cb0ae6eba54a161f7234db1ea
Gianpaolo <ravndeveloper@gmail.com>
2021-01-13 17:21:37 +0100

    Update pre_works.php

--- pre_works.php
+++ pre_works.php
@@ -1,60 +1,61 @@
 <!DOCTYPE html>
 <html class="has-navbar-fixed-top" lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta content="width=device-width, initial-scale=1" name="viewport" />
+<head>
+    <meta charset="UTF-8"/>
+    <meta content="width=device-width, initial-scale=1" name="viewport"/>
     <title>Works</title>
-    <link href="css/bulma.min.css" rel="stylesheet" />
-    <link href="css/pre_works.css" rel="stylesheet" type="text/css" />
-    <link href="css/bello.css" rel="stylesheet" type="text/css" />
-    <link href="css/menu.css" rel="stylesheet" type="text/css" />
+    <link href="css/bulma.min.css" rel="stylesheet"/>
+    <link href="css/pre_works.css" rel="stylesheet" type="text/css"/>
+    <link href="css/bello.css" rel="stylesheet" type="text/css"/>
+    <link href="css/menu.css" rel="stylesheet" type="text/css"/>
     <link
-      href="https://cdn.jsdelivr.net/npm/bulma-carousel@4.0.4/dist/css/bulma-carousel.min.css"
-      rel="stylesheet"
+            href="https://cdn.jsdelivr.net/npm/bulma-carousel@4.0.4/dist/css/bulma-carousel.min.css"
+            rel="stylesheet"
     />
-    <link href="https://vjs.zencdn.net/7.10.2/video-js.css" rel="stylesheet" />
+    <link href="https://vjs.zencdn.net/7.10.2/video-js.css" rel="stylesheet"/>
     <script
-      crossorigin="anonymous"
-      src="https://kit.fontawesome.com/9eeab300fc.js"
+            crossorigin="anonymous"
+            src="https://kit.fontawesome.com/9eeab300fc.js"
     ></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.5.1/gsap.min.js"></script>
-  </head>
-  <body style="background-color: #111">
+</head>
+<body style="background-color: #111">
 
-    <?php require "components/navbar.php";?>
+<?php require "components/navbar.php"; ?>
 
-    <?php require "components/menu.php";?>
+<?php require "components/menu.php"; ?>
 
 <div id='mainSection' class="grained">
     <header>
-<div class="makuda-home-link--works makuda-home-link-wrap is-loaded" style="top: 100%;">
-    <div class="makuda-home-link makuda-home-link-works is-link ">
-      <a id="text-case-study">
-        <span class='noWrap'>strategia, branding, management, social media</span>
-        <span class='noWrap'>case study</span>
-      </a>
-    </div>
-</div>
-</header>
-<header>
-<div class="makuda-home-link--contacts makuda-home-link-wrap is-loaded" style="top: 100%;">
-    <div class="makuda-home-link is-link ">
-        <a id="text-produzioni">
-            <span class='noWrap'>PRODUZIONI</span>
-            <span class='noWrap'>cinematic & spot adv, shooting, 3D VFX</span>
-        </a>
-    </div>
-</div>
-</header>
+        <div class="makuda-home-link--works makuda-home-link-wrap is-loaded" style="top: 100%;">
+            <div class="makuda-home-link makuda-home-link-works is-link ">
+                <a id="text-case-study">
+                    <span class='noWrap'>strategia, branding, management, social media</span>
+                    <span class='noWrap'>case study</span>
+                </a>
+            </div>
+        </div>
+    </header>
+    <header>
+        <div class="makuda-home-link--contacts makuda-home-link-wrap is-loaded" style="top: 100%;">
+            <div class="makuda-home-link is-link ">
+                <a id="text-produzioni">
+                    <span class='noWrap'>PRODUZIONI</span>
+                    <span class='noWrap'>cinematic & spot adv, shooting, 3D VFX</span>
+                </a>
+            </div>
+        </div>
+    </header>
 
 
     <div id="video-compare-container">
         <div class="mask">
             <video id="video2"
-
+                   preload="auto"
                    loop
                    muted
+                   autoplay
             >
                 <source
                         src='/media/video/A Story Of Matteo web Optimization.mp4'>
@@ -62,28 +63,29 @@
             </video>
         </div>
 
-    <div id="video-clipper">
-      <video id="video1"
+        <div id="video-clipper">
+            <video id="video1"
+                   preload="auto"
+                   loop
+                   muted
+                   autoplay
+            >
+                <source
+                        src='/media/video/Suites for Web.mp4'>
 
-        loop
-        muted
-      >
-        <source
-         src='/media/video/Suites for Web.mp4'>
-
-      </video>
+            </video>
+        </div>
     </div>
-  </div>
 </div>
 </div>
-    <script
-      crossorigin="anonymous"
-      integrity="sha512-6MT8e40N5u36Um5SXKtwZmoKcCSg1XaKtexnXZPpQ4iJDHrBEHXKz37fnDovXezsaCd4oKCH5Y+vrcl7qpLPoA=="
-      src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.5.1/CSSRulePlugin.min.js"
-    ></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.5.1/EasePack.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.5.1/ScrollToPlugin.min.js"></script>
-    <script src="js/menu.js" type="text/javascript"></script>
-    <script src='js/pre_works.js' type='text/javascript'></script>
-  </body>
+<script
+        crossorigin="anonymous"
+        integrity="sha512-6MT8e40N5u36Um5SXKtwZmoKcCSg1XaKtexnXZPpQ4iJDHrBEHXKz37fnDovXezsaCd4oKCH5Y+vrcl7qpLPoA=="
+        src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.5.1/CSSRulePlugin.min.js"
+></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.5.1/EasePack.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.5.1/ScrollToPlugin.min.js"></script>
+<script src="js/menu.js" type="text/javascript"></script>
+<script src='js/pre_works.js' type='text/javascript'></script>
+</body>
 </html>
