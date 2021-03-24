dabfe8fc40175919df1a6277759d226f96a157de
Napolitano Andrea <napolitanoandrea00@gmail.com>
2021-03-24 11:24:01 +0100

    Modifiche import generali

--- components/common_libraries.php
+++ components/common_libraries.php
@@ -1,16 +1,24 @@
-<?php
-echo "<!-- Google Tag Manager -->
-<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-TD8SSC7');</script>
-<!-- End Google Tag Manager -->";
-echo '<meta name="viewport" content="width=device-width, initial-scale=1">
+<!-- Google Tag Manager -->
+<script>
+(function(w,d,s,l,i){
+    w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});
+    var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
+        j.async=true;
+        j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+        f.parentNode.insertBefore(j,f);
+});
+(window,document,'script','dataLayer','GTM-TD8SSC7');
+gtag('config', 'AW-397981285');
+</script>
+<!-- Event snippet for Website lead conversion page -->
+<script> gtag('event', 'conversion', {'send_to': 'AW-397981285/K_ReCN_18vwBEOXs4r0B'}); </script>
+<!-- End Google Tag Manager -->
+<meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="icon" type="image/png" href="/favicon.ico"/>
 <link href="/css/bulma.min.css" rel="stylesheet"/>
 <link href="/css/menu.css" rel="stylesheet" type="text/css"/>
 <link href="/css/bulma-carousel.min.css" rel="stylesheet"/>
 <script src="/js/jquery.min.js"></script>
 <script src="/js/jquery-ui.min.js"></script>
-<script src="/js/gsap.min.js"></script>';
+<script src="/js/gsap.min.js"></script>
