e2153f47bf861b866076e7b187616029eca7161f
Napolitano Andrea <napolitanoandrea00@gmail.com>
2021-03-28 11:19:36 +0200

    Tornato indietro in un file

--- components/common_libraries.php
+++ components/common_libraries.php
@@ -1,5 +1,12 @@
 <!-- Google Tag Manager -->
-<script>
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-TD8SSC7');</script>
+<!-- End Google Tag Manager -->
+<!-- Google Tag Manager (NUOVO) -->
+<!--<script>
 (function(w,d,s,l,i){
     w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});
     var f=d.getElementsByTagName(s)[0],
@@ -9,7 +16,7 @@
         f.parentNode.insertBefore(j,f);
 });
 (window,document,'script','dataLayer','GTM-TD8SSC7')//AW-397981285
-</script>
+</script> -->
 <!-- End Google Tag Manager gtag('config', 'AW-397981285'); -->
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="icon" type="image/png" href="/favicon.ico"/>
