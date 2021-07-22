651764edb1f712147f6e6ce2a6d25208f682e74f
Napolitano Andrea <napolitanoandrea00@gmail.com>
2021-07-22 21:36:50 +0200

    Shopping Live con foto di Matteo Toia :D

---
+++ lavori/shopping_live.php
@@ -0,0 +1,525 @@
+<!DOCTYPE html>
+
+<?php
+require('workPHP.php');
+$view = ButtonBeforeAfter("SHOPPING");
+?>
+<html class="has-navbar-fixed-top" lang="en">
+
+<head>
+    <meta charset="UTF-8"/>
+    <title>SHOPPING LIVE - Makuda Consulting</title>
+    <?php require('../components/common_libraries.php') ?>
+    <link href="/css/work.css" rel="stylesheet" type="text/css"/>
+    <link href="/css/bello.css" rel="stylesheet" type="text/css"/>
+    <link href="/css/menu.css" rel="stylesheet" type="text/css"/>
+    <link href="/css/animations.css" rel="stylesheet" type="text/css"/>
+</head>
+
+<body style="background-color: #111; min-height: 100vh">
+<?php require('../components/libraries_after_body.php') ?>
+<style>
+    @media only screen and (max-width: 1024px) {
+        .subTitleVideo:before {
+            content: '\a';
+            white-space: pre;
+        }
+    }
+
+    #img1 {
+        background-image: url(/media/img/lavori/toia_1.jpg);
+
+    }
+
+    #img2 {
+
+        background-image: url(/media/img/lavori/toia_2.jpg);
+    }
+
+    #img3 {
+        background-image: url(/media/img/lavori/toia_3.jpg);
+    }
+
+    #img4 {
+        background-image: url(/media/img/lavori/toia_4.jpg);
+    }
+
+    #img5 {
+        background-image: url(/media/img/lavori/toia_5.jpg);
+    }
+
+    #img6 {
+        background-image: url(/media/img/lavori/toia_6.jpg);
+    }
+</style>
+
+<?php require("../components/navbar.php"); ?>
+<?php require("../components/menu.php"); ?>
+
+<div class="mainSection">
+
+    <section class="sectionFullWorks">
+
+        <div class="workList">
+
+            <div class="workListItem">
+
+                <div class="container">
+
+                    <div class="workBlockContainer">
+
+                        <div class="workBlock">
+
+                            <div class="workBlockImageSub" style="opacity: 100%">
+
+                                <div class="container is-max-desktop">
+
+                                    <video autoplay class=" video-js vjs-default-skin vjs-16-9"
+
+                                           data-setup='{"controls":true}' loop muted preload="none">
+
+                                        <source src="/media/video/works/Shopping-1080p.mp4" type="video/mp4" label='1080p'/>
+
+                                        <p class="vjs-no-js">
+
+                                            To view this video please enable JavaScript, and
+
+                                            consider upgrading to a web browser that
+
+                                            <a href="https://videojs.com/html5-video-support/"
+
+                                               target="_blank">supports HTML5 video</a>
+
+                                        </p>
+
+                                    </video>
+
+                                </div>
+
+                            </div>
+
+                        </div>
+
+                    </div>
+
+                    <div class="container textContainer">
+
+                        <div class='singleWorkBlockTitle'>
+
+                            <p class="titleVideo noWrap" style="font-size: 5em">
+
+                                <?= $view["title"] ?>
+
+                                <span class="subTitleVideo makuda-color"
+
+                                      style="font-size:0.5em;"><?= $view["subtitle"] ?></span>
+
+                            </p>
+
+                        </div>
+
+                        <br/>
+
+                        <div id="description">
+
+                        </div>
+
+                        <br/>
+
+                        <p class="subVideo">IL PROGETTO</p>
+
+                        <br/>
+
+                        <div class="columns is-6">
+                            <div class="column is-two-thirds">
+                                <p style="color: white;">
+                                Shopping-Live è una proprietà di Alpac Srl che ha sviluppato un rivoluzionario portale realizzato grazie la quale è possibile fare acquisti ovunque ci si trovi, entrando "live" nello store desiderato in ogni parte del mondo.<br>
+                                In maniera molto semplice è possibile entrare in videochiamata con i personal shopper degli store, interagendo con loro e vedendo attraverso i loro occhi grazie a degli occhiali di ultimissima tecnologia.
+                                </p>
+                            </div>
+                            <div class="column subVideoDescription">
+                                <div class="columns is-mobile">
+                                    <div class="column"><p class="boldOrange">Cliente: </p></div>
+                                    <div class="column"><p>Alpac srl</p></div>
+                                </div>
+                                <div class="columns is-mobile">
+                                    <div class="column"><p><span class="boldOrange">Regia: </span></p>
+                                    </div>
+                                    <div class="column"><p>
+                                            Simone Santacroce <br> Taras Snegovoy</p></div>
+                                </div>
+                                <div class="columns is-mobile">
+                                    <div class="column"><p class="boldOrange">Anno: </p></div>
+                                    <div class="column"><p>2021</p></div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</div>
+<div class="pubblicazione-social makuda-bgcolor">
+    <div class="container">
+        <p class="title" style="margin-bottom:10px;padding-top:40px;">
+            PRODUZIONE E
+            <span class="black-font">MONTAGGIO</span>
+        </p>
+
+        <p class="frase">
+            Per la realizzazione di questo spot si è resa necessario un lavoro di pre produzione molto curato, tipico delle piccole - medie  produzioni.
+            Sceneggiatura, storyboard, scouting, scelta delle location e relativi props: ogni step è stato curato al dettaglio.<br><br>
+
+            Sul set è stato realizzato un importante lavoro di preparazione del cast e degli ambienti aiutati da ruoli come M.U.A. , Stylist, e D.O.P. che ha concretizzato un lightining design apposito.<br><br>
+
+            In post-produzione abbiamo realizzato un montaggio relativamente semplice,  grazie a stoyboard, sceneggiatura, ciak e doppiatore.
+            Sono stati aggiunti piccoli effetti, tra cui motion graphic di smartphone che spiegassero meglio il servizio, e infine abbiamo colorato lo spot mantenendo un look neutrale / commercial
+        </p>
+
+        <div class="gif-firstSection">
+
+
+
+            <div class="columns" style="padding-top: 50px">
+
+                <div class="column">
+
+                    <video autoplay class=" video-js vjs-default-skin vjs-16-9"
+
+                           data-setup='{"controls":false}' loop muted preload="none">
+
+                        <source src="/media/video/works/BackstageMakuda%20for%20web.mp4" type="video/mp4"/>
+
+                        <p class="vjs-no-js">
+
+                            To view this video please enable JavaScript, and
+
+                            consider upgrading to a web browser that
+
+                            <a href="https://videojs.com/html5-video-support/"
+
+                               target="_blank">supports HTML5 video</a>
+
+                        </p>
+
+                    </video>
+
+                    <p class="frase" style="padding-top: 20px">
+
+                        Video Backstage realizzato da Mattia Oliva
+
+                        <br>
+
+
+
+                    </p>
+
+                </div>
+
+                <div class="column">
+
+                    <video autoplay class=" video-js vjs-default-skin vjs-16-9"
+
+                           data-setup='{"controls":false}' loop muted preload="none"
+
+                           style="background-color: transparent;">
+
+                        <source src="/media/video/works/Time%20Line%2016-9web.mp4"
+
+                                type="video/mp4"/>
+
+                        <p class="vjs-no-js">
+
+                            To view this video please enable JavaScript, and
+
+                            consider upgrading to a web browser that
+
+                            <a href="https://videojs.com/html5-video-support/"
+
+                               target="_blank">supports HTML5 video</a>
+
+                        </p>
+
+                    </video>
+
+                    <p class="frase" style="padding-top: 20px">
+
+                        La complessa timeline del progetto
+
+                        <br>
+
+                    </p>
+
+                </div>
+
+            </div>
+
+
+
+        </div>
+
+        <br>
+
+        <br>
+
+    </div>
+
+</div>
+
+
+
+
+
+<div class="container imgs-slide">
+
+    <p class="title" style="margin-bottom:10px;">
+
+        Qualche Nostro <span class="makuda-color">Scatto</span>
+
+    </p>
+
+    <p class="frase" style='color: white; text-align:center'>
+
+        Eccoci in azione, di seguito qualche nostro scatto del BTS del video.
+
+    </p>
+
+    <br/>
+
+
+
+    <div class="columns is-mobile gif-photo">
+
+        <div class="column is-one-third-desktop">
+
+            <div class="img-slide">
+
+                <div id="img1" class="small-image image-div"></div>
+
+            </div>
+
+        </div>
+
+        <div class="column is-one-third-desktop">
+
+            <div class="img-slide ">
+
+                <div id="img2" class="big-image image-div"></div>
+
+            </div>
+
+        </div>
+
+        <div class="column is-one-third-desktop">
+
+            <div class="img-slide">
+
+                <div id="img3" class="small-image image-div"></div>
+
+            </div>
+
+        </div>
+
+        <div class="column is-one-third-desktop">
+
+            <div class="img-slide">
+
+                <div id="img4" class="big-image image-div"></div>
+
+            </div>
+
+        </div>
+
+        <div class="column is-one-third-desktop">
+
+            <div class="img-slide">
+
+                <div id="img5" class="small-image image-div"></div>
+
+            </div>
+
+        </div>
+
+        <div class="column is-one-third-desktop">
+
+            <div class="img-slide">
+
+                <div id="img6" class="big-image image-div"></div>
+
+            </div>
+
+        </div>
+
+    </div>
+
+</div>
+
+
+
+<div class="full-screen-carousel" style="display:none;">
+
+    <div class="full-screen-carousel-background"></div>
+
+    <div class="full-screen-carousel-container">
+
+        <div id="img1" class="full-screen-carousel-image"></div>
+
+        <div id="img2" class="full-screen-carousel-image"></div>
+
+        <div id="img3" class="full-screen-carousel-image"></div>
+
+        <div id="img4" class="full-screen-carousel-image"></div>
+
+        <div id="img5" class="full-screen-carousel-image"></div>
+
+        <div id="img6" class="full-screen-carousel-image"></div>
+
+
+
+    </div>
+
+    <div class="full-screen-carousel-control left">
+
+        <div class="full-screen-carousel-arrow">
+
+            <i class="fas fa-chevron-left"></i>
+
+        </div>
+
+    </div>
+
+    <div class="full-screen-carousel-control right">
+
+        <div class="full-screen-carousel-arrow right">
+
+            <i class="fas fa-chevron-right"></i>
+
+        </div>
+
+    </div>
+
+
+
+    <a class='cross'>
+
+        <i class="bi bi-plus" style="font-size: 3em"></i>
+
+    </a>
+
+
+
+</div>
+
+<br><br>
+
+<?php
+
+button('TOIA');
+
+?>
+
+
+
+<?php require "../components/footer.php" ?>
+
+<?php require "../components/works_libraries.php" ?>
+
+<script src="workJS.js" type="text/javascript"></script>
+
+<script src="/js/menu.js" type="text/javascript"></script>
+
+<script src="/js/aboutJS.js" type="text/javascript"></script>
+
+<script src="/js/wavesurfer.js"></script>
+
+<script>
+
+
+
+    $(function () {
+
+
+
+
+
+        let wavesurfer;
+
+
+
+        var AudioContext = window.AudioContext // Default
+
+            || window.webkitAudioContext // Safari and old versions of Chrome
+
+            || false;
+
+
+
+        if (AudioContext) {
+
+            var buttonPlay = $('.playMakuda');
+
+            wavesurfer = WaveSurfer.create({
+
+                container: '#waveform',
+
+                waveColor: 'black',
+
+                progressColor: 'black'
+
+            });
+
+            wavesurfer.load('/media/Audio/AudioToia.mp3');
+
+            buttonPlay.on('click', function () {
+
+                var context = new AudioContext();
+
+                context.resume().then(() => {
+
+                    wavesurfer.playPause();
+
+                    buttonPlay.toggleClass('bi-play bi-pause');
+
+                })
+
+            });
+
+        } else {
+
+            var buttonPlay = $('.playMakuda');
+
+            wavesurfer = WaveSurfer.create({
+
+                container: '#waveform',
+
+                waveColor: 'black',
+
+                progressColor: 'black'
+
+            });
+
+            wavesurfer.load('/media/Audio/AudioCiroc.mp3');
+
+            buttonPlay.on('click', function () {
+
+                wavesurfer.playPause();
+
+                buttonPlay.toggleClass('bi-play bi-pause');
+
+            })
+
+        }
+
+    })
+
+
+
+
+
+</script>
+
+</body>
+
+</html>
+
